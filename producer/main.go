1213b69b11c1288df68b528953da176e9f1a9c77
juniorpb <juniorfoxld@hotmail.com>
2020-09-29 11:31:21 -0300

    create producer

---
+++ producer/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	kafka "github.com/segmentio/kafka-go"
+)
+
+func newKafkaWriter(kafkaURL, topic string) *kafka.Writer {
+	return kafka.NewWriter(kafka.WriterConfig{
+		Brokers:  []string{kafkaURL},
+		Topic:    topic,
+		Balancer: &kafka.LeastBytes{},
+	})
+}
+
+func main() {
+
+	kafkaURL := "localhost:9092"
+	topic := "topic-teste"
+	writer := newKafkaWriter(kafkaURL, topic)
+	defer writer.Close()
+	fmt.Println("start producing ... !!")
+
+	for i := 0; ; i++ {
+		msg := kafka.Message{
+			//Key:   []byte(fmt.Sprintf("Key-%d", i)),
+			Value: []byte(fmt.Sprint("aaaaa")),
+		}
+
+		err := writer.WriteMessages(context.Background(), msg)
+
+		if err != nil {
+			fmt.Println("Erro ao escrever mensagem", err)
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+}
